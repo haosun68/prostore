3ba577ede65aaf9ed8e00bd433d40dae9bd9034d
haosun68 <haosun8686@gmail.com>
2025-07-10 02:06:04 -0500

    feat: Add Header and Footer components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,7 @@
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">  
+      <Header />
       <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   );
 }
