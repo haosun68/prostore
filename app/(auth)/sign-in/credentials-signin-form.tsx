3915a20fc4f91847f29eb7869840c4c7d54c806e
haosun68 <haosun8686@gmail.com>
2025-07-20 02:52:49 -0500

    feat: Add user sign-in functionality

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -5,10 +5,33 @@ import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { signInDefaultValues } from '@/lib/constants';
+import { signInWithCredentials } from '@/lib/actions/user.actions';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
+import { useSearchParams } from 'next/navigation';
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: ''
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className='w-full' variant='default'>
+        { pending ? 'Signing In...' : 'Sign In' }
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={action}>
+      <input type='hidden' name='callbackUrl' value={callbackUrl} />
       <div className='space-y-6'>
         <div>
           <Label htmlFor='email'>Email</Label>
@@ -34,10 +57,15 @@ const CredentialsSignInForm = () => {
         </div>
       </div>
       <div>
-        <Button className='w-full' variant='default'>
-          Sign In
-        </Button>
+        <SignInButton />
       </div>
+      
+      {data && !data.success && (
+        <div className='text-center text-destructive'>
+          { data.message }
+        </div>
+      )}
+      
       <div className='text-sm text-center text-muted-foreground'>
         Don&apos;t have an account?{' '}
         <Link href='/sign-up' target='_self' className='link'>
