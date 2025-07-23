8ab12f16a0ab46a22664510d4ad51b3b4fff3cf1
haosun68 <haosun8686@gmail.com>
2025-07-23 01:25:50 -0500

    feat: add cart page and shipping address form

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -22,3 +22,11 @@ export const signUpDefaultValues = {
   confirmPassword: '',
 };
 
+export const shippingAddressDefaultValues = {
+  fullName: 'John Doe',
+  streetAddress: '123 Main st',
+  city: 'Anytown',
+  postalCode: '12345',
+  country: 'USA',
+};
+
