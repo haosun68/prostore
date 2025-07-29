e7236d4ba9b6e42a95bbb4e1f7d832d2a0714b8d
haosun68 <haosun8686@gmail.com>
2025-07-29 00:00:59 -0500

    feat(admin): Implement dashboard overview and sales chart

---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,117 @@
+import { auth } from '@/auth';
+import { Metadata } from 'next';
+import { getOrderSummary } from '@/lib/actions/order.actions';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from '@/components/ui/table';
+import { BadgeDollarSign, CreditCard, Users, Barcode } from 'lucide-react';
+import Link from 'next/link';
+import { formatCurrency, formatNumber, formatDateTime } from '@/lib/utils';
+import Charts from './charts';
+
+export const metadata: Metadata = {
+  title: 'Admin Dashboard',
+};
+
+const AdminOverviewPage = async () => {
+  const session = await auth();
+
+  if (session?.user?.role !== 'admin') {
+    throw new Error('User is not authorized')
+  }
+
+  const summary = await getOrderSummary();
+
+
+  return <div className='space-y-2'>
+    <h1 className="h2-bold">Dashboard</h1>
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+      <Card>
+        <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+          <CardTitle className='text-sm font-medium'>Total Revenue</CardTitle>
+          <BadgeDollarSign />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {formatCurrency(summary.totalSales._sum.totalPrice!.toString())}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+          <CardTitle className='text-sm font-medium'>Sales</CardTitle>
+          <CreditCard />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {formatNumber(summary.ordersCount)}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+          <CardTitle className='text-sm font-medium'>Customers</CardTitle>
+          <Users />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {formatNumber(summary.usersCount)}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+          <CardTitle className='text-sm font-medium'>Products</CardTitle>
+          <Barcode />
+        </CardHeader>
+        <CardContent>
+          <div className="text-2xl font-bold">
+            {formatNumber(summary.productsCount)}
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+      <Card className='col-span-4'>
+        <CardHeader>
+          <CardTitle>Overview</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Charts data={{ salesData: summary.salesData, }} />
+        </CardContent>
+      </Card>
+      <Card className='col-span-3'>
+        <CardHeader>
+          <CardTitle>Recent Sales</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>BUYER</TableHead>
+                <TableHead>DATE</TableHead>
+                <TableHead>TOTAL</TableHead>
+                <TableHead>ACTIONS</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {summary.latestSales.map((sale) => (
+                <TableRow key={sale.id}>
+                  <TableCell>{sale.user.name}</TableCell>
+                  <TableCell>{formatDateTime(sale.createdAt).dateOnly}</TableCell>
+                  <TableCell>{formatCurrency(sale.totalPrice)}</TableCell>
+                  <TableCell>
+                    <Link href={`/order/${sale.id}`}>
+                      <span className="px-2">Details</span>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
+  </div>;
+};
+
+export default AdminOverviewPage; 
--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,5 +1,6 @@
 'use server';
 
+type SalesDataType = Array<{ month: string; totalSales: number }>;
 
 import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
@@ -12,6 +13,7 @@ import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PaymentResult } from "@/types";
 import { PAGE_SIZE } from "../constants";
+import { Prisma } from "@prisma/client";
 
 
 // Create order and create the order items
@@ -239,7 +241,7 @@ async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error('Order not found');
-}
+} 
 
 // Get user's orders
 export async function getMyOrders({
@@ -266,5 +268,50 @@ export async function getMyOrders({
   return {
     data,
     totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+type SalesDataType = {
+  month: string;
+  totalSales: number;
+}[];
+
+// Get order summary for admin dashboard
+export async function getOrderSummary() {
+  // Get counts for each resource
+  const ordersCount = await prisma.order.count();
+  const productsCount = await prisma.product.count();
+  const usersCount = await prisma.user.count();
+
+  // Calculate the total sales
+  const totalSales = await prisma.order.aggregate({
+    _sum: { totalPrice: true }
+  });
+
+  // Get monthly sales
+  const salesDataRaw = await prisma.$queryRaw<
+    Array<{ month: string; totalSales: Prisma.Decimal }>
+  >`SELECT to_char("createdAt", 'MM/YY') as "month", sum("totalPrice") as "totalSales" FROM
+"Order" GROUP BY to_char("createdAt", 'MM/YY')`;
+
+  const salesData: SalesDataType = salesDataRaw.map((entry) => ({
+    month: entry.month,
+    totalSales: Number(entry.totalSales),
+  }));
+
+  // Get latest sales
+  const latestSales = await prisma.order.findMany({
+    orderBy: { createdAt: 'desc' },
+    include: { user: { select: { name: true } }, },
+    take: 6,
+  });
+
+  return {
+    ordersCount,
+    productsCount,
+    usersCount,
+    totalSales,
+    latestSales,
+    salesData,
   };
 } 
